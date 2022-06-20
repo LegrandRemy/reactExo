9f9d291dbcb54daec47a384e1823ae7eee2d1538
LegrandRemy <legrandremy19@outlook.fr>
2022-06-20 15:46:51 +0200

    nouveau

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+import toto from './tp.json'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // <View style={{flex:1}}>     EXEMPLE DE BOUCLE MAP
+    // {toto.map((item,index)=>{
+    //   return(
+    //     <Text key={index}>
+    //       {item.title}
+    //     </Text>
+    //   )
+    // })}
+    // </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      {toto.map((item, index) => {
+        console.log(item)
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <View
+              style={{
+                paddingTop: 20,
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Image source={{ uri: item.img }} style={styles.img}></Image>
+            </View>
+            <View style={styles.blocText}>
+              <Text key={index} style={styles.title}>
+                {item.title}
+              </Text>
+              <Text style={styles.text}>{item.desc}</Text>
+            </View>
+          </View>
+        )
+      })}
+    </ScrollView>
+  )
 }
+
+//     <View style={styles.container}>
+//       <View style={{ flexDirection: 'row',flex:1}}>
+//       <View style={{paddingTop:20, flex:1}}>
+//         <Image source={{uri: toto[0].img}} style={styles.img}></Image></View>
+//         <View style={styles.blocText}>
+//           <Text style={styles.title}>{toto[0].title}</Text>
+//           <Text style={styles.text}>{toto[0].desc}</Text>
+//           </View>
+//       </View>
+//       <View style={{ flexDirection: 'row',flex:1}}>
+//       <View style={{paddingTop:20, flex:1}}>
+//         <Image source={{uri: toto[1].img}} style={styles.img}></Image></View>
+//         <View style={styles.blocText}>
+//           <Text style={styles.title}>{toto[1].title}</Text>
+//           <Text style={styles.text}>{toto[1].desc}</Text>
+//           </View>
+//       </View>
+//       <View style={{ flexDirection: 'row',flex:1}}>
+//         <View style={{paddingTop:20, flex:1}}>
+//         <Image source={{uri: toto[2].img}} style={styles.img}></Image></View>
+//         <View style={styles.blocText}>
+//           <Text style={styles.title}>{toto[2].title}</Text>
+//           <Text style={styles.text}>{toto[2].desc}</Text>
+//           </View>
+//       </View>
+//       <View style={{ flexDirection: 'row',flex:1}}>
+//       <View style={{paddingTop:20, flex:1}}>
+//         <Image source={{uri: toto[3].img}} style={styles.img}></Image></View>
+//         <View style={styles.blocText}>
+//           <Text style={styles.title}>{toto[3].title}</Text>
+//           <Text style={styles.text}>{toto[3].desc}</Text>
+//           </View>
+//       </View>
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img: {
+    height: 200,
+    width: '100%',
+    resizeMode: 'contain',
+  },
+  title: {
+    color: 'red',
+  },
+  blocText: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    padding: 10,
+  },
+})
